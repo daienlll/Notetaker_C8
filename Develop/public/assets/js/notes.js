6629db0d3454ce0120d8823afc27e0483d0a773d
Vanguardian <antoinedmichels@gmail.com>
2023-05-14 15:45:12 -0500

    initial commit before testing with Heroku. this contains all the db and js files

---
+++ Develop/public/assets/js/notes.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+
+function createnotes(body, notesarray) {
+    fs.writeFileSync(
+        path.join(__dirname, '../../../db/db.json'),
+        JSON.stringify({ notesarray.push(body) }, null, 2)
+    );
+
+    return body;
+};
+
+function deletenote(id, notes) {
+    let notesarray = notes.filter(el => {
+        if (el.id == id) {
+            return false
+        } else {
+            return true
+        }
+    }) 
+
+    let index = 0;
+    notesarray.forEach(note => {
+        note.id = index;
+        index += 1;
+    });
+
+    fs.writeFileSync(
+        path.join(__dirname, '../../../db/db.json'),
+        JSON.stringify({ notesarray }, null, 2)
+    );
+    return notesarray;
+}
+
+module.exports = {
+    createnotes,
+    deletenote
+};
---
+++ server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const apiRoutes = apirouter();
+const htmlRoutes = htmlrouter();
+
+function apirouter() {
+    const router = express.Router();
+    const { createnotes, deletenote } = require('Develop/public/assets/js/notes.js');
+    let { notesarray } = require('Develop/db/db.json');
+
+    router.get('/notes', (req, res) => {
+        res.json(notesarray);
+    });
+
+    router.post('/notes', (req, res) => {
+        if (notesarray) {
+            req.body.id = notesarray.length.toString();
+        } else { req.body.id = 0 }
+        res.json(createnotes(req.body, notesarray));
+    });
+
+    router.delete('/notes/:id', async (req, res) => {
+        const { id } = req.params
+        notesarray = await deletenote(id, notesarray);
+        res.json(notesarray);
+    });
+
+}
+
+function htmlrouter() {
+    const router = express.Router();
+
+    router.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, 'public/index.html'));
+    });
+
+    router.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, 'public/notes.html'));
+    });
+
+    router.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'public/index.html'));
+    });
+
+    return router;
+};
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
