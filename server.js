09ad8251dda7b3ed4be6550b200b113198ee9066
Vanguardian <antoinedmichels@gmail.com>
2023-05-14 19:07:42 -0500

    Throwing more stuff at more walls

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const { createnotes, deletenote } = require(`./Develop/public/assets/js/notes.js`);
-let { notesarray } = require('./Develop/db/db.json');
+const { createnotes, deletenote } = require('./Develop/public/assets/js/notes.js');
+var notesarray = require('./Develop/db/db.json');
+console.log(notesarray)
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -11,7 +13,9 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
     res.json(notesarray);
+    console.log(notesarray)
 });
 
 app.post('/notes', (req, res) => {
@@ -19,24 +23,28 @@ app.post('/notes', (req, res) => {
         req.body.id = notesarray.length.toString();
     } else { req.body.id = 0 }
     res.json(createnotes(req.body, notesarray));
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
 });
 
 app.delete('/notes/:id', async (req, res) => {
     const { id } = req.params
     notesarray = await deletenote(id, notesarray);
     res.json(notesarray);
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, `./Develop/public/index.html`));
+    pathway = path.join(__dirname, './Develop/public/index.html')
+    console.log(pathway)
+    res.sendFile(pathway);
 });
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, `./Develop/public/notes.html`));
-});
+// app.get('/notes', (req, res) => {
+    
+// });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, `./Develop/public/index.html`));
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
 });
 
 app.listen(PORT, () => {
